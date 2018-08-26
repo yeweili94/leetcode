cc78a765f0f32f8639c7906b78a3b805b35d26ed
yeweili <yeweili94@gmail.com>
2018-08-26 19:46:18 +0800

    add best solution

--- array/55.jump-game.0.cpp
+++ array/55.jump-game.0.cpp
@@ -2,29 +2,12 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int size = nums.size();
-        if (size <= 1) {
-            return true;
-        }
-        queue<int> queue;
-        queue.push(0);
-        while (!queue.empty()) {
-            int start = queue.front();
-            int max_jump = 0;
-            int num = queue.size();
-            for (int i = 0; i < num; i++) {
-                int tmp = queue.front();
-                queue.pop();
-                if (nums[tmp] + tmp > max_jump) {
-                    max_jump = tmp + nums[tmp];
-                }
-            }
-            if (max_jump >= size - 1) {
-                return true;
-            }
-            for (int i = start + num; i <= max_jump; i++) {
-                queue.push(i);
+        int good_position = size - 1;
+        for (int i = good_position - 1; i >= 0; i--) {
+            if (i + nums[i] >= good_position) {
+                good_position = i;
             }
         }
-        return false;
+        return good_position == 0;
     }
 };
