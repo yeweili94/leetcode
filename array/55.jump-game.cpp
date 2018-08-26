0f78a86b108131bc23a67954cbc7aea3b9164f5b
yeweili <yeweili94@gmail.com>
2018-08-26 19:10:02 +0800

    add solution

--- array/55.jump-game.cpp
+++ array/55.jump-game.cpp
@@ -1,36 +1,27 @@
-#include <iostream>
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int size = nums.size();
-        if (size <= 1) return true;
-        queue<int> queue;
-        queue.push(0);
-        int max_jump = 0;
-        vector<bool> used(size, false);
-        used[0] = true;
-        while (!queue.empty()) {
-            int n = queue.size();
-            for (int i = 0; i < n; i++) {
-                int tmp = queue.front();
-                queue.pop();
-                if (tmp >= size - 1) {
-                    return true;
-                }
-                if (nums[tmp] + tmp > max_jump) {
-                    max_jump = tmp + nums[tmp];
-                    if (max_jump >= size - 1) {
-                        return true;
-                    }
-                    for (int j = tmp + 1; j <= max_jump; j++) {
-                        if (!used[j]) {
-                            queue.push(j);
-                            used[j] = true;
-                        }
-                    }
+        if (size <= 1) {
+            return true;
+        }
+        int left = 0, right = 0;
+        while (left < size && right < size) {
+            int max_jump = 0;
+            if (left >= size - 1 || right >= size - 1) {
+                return true;
+            }
+            for (int i = left; i <= right; i++) {
+                if (max_jump < i + nums[i]) {
+                    max_jump= i + nums[i];
                 }
             }
+            if (max_jump <= right) {
+                return false;
+            }
+            left = right + 1;
+            right = max_jump;
         }
-        return false;
+        return true;
     }
-} ;
+};
